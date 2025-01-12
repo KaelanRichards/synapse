3bbf10298a553f9d35c01f89b2500d1e34542c7b
kadokaelan <kadokaelan@gmail.com>
2025-01-11 20:35:29 -0800

    Refactor Supabase client configuration to utilize getEnvVar for environment variables, improving error handling and code readability. Removed direct environment variable checks and streamlined the client initialization process.

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,16 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
-
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
-}
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
-}
+import { getEnvVar } from "../env";
 
 export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  getEnvVar("NEXT_PUBLIC_SUPABASE_URL"),
+  getEnvVar("NEXT_PUBLIC_SUPABASE_ANON_KEY"),
   {
     auth: {
       persistSession: true,
@@ -20,13 +14,9 @@ export const supabase = createClient<Database>(
 );
 
 export const getServiceSupabase = () => {
-  if (!process.env.SUPABASE_SERVICE_ROLE_KEY) {
-    throw new Error("Missing env.SUPABASE_SERVICE_ROLE_KEY");
-  }
-
   return createClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    getEnvVar("NEXT_PUBLIC_SUPABASE_URL"),
+    getEnvVar("SUPABASE_SERVICE_ROLE_KEY"),
     {
       auth: {
         autoRefreshToken: false,
